e4a1eadbd459e27930026efd2442caea8e02a725
MonTicket <monticket2@gmail.com>
2023-11-21 00:16:55 +0100

    initial commit

---
+++ app/(home)/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomeLayout({children}) {
+  return (
+    <div>
+        HomeLayout Page...
+        {children}
+    </div>
+  )
+}
+
+export default HomeLayout
